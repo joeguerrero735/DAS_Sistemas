c4e2c5e487477599154ed0c21e8b6db441d82491
joe zuñiga guerrero <joesito_7@hotmail.com>
2022-01-25 22:11:48 -0600

    agrego practica 1.5

--- Ene-Jun-2022/joe-marco-zuniga-guerrero/practica-1/main-1.py
+++ Ene-Jun-2022/joe-marco-zuniga-guerrero/practica-1/main-1.py
@@ -1,3 +1,10 @@
-x=10
-print(x-5)
+x = 10
+print("x =", x)
 
+print ("x + 5 =", x + 5)
+print ("x - 5 =", x - 5)
+print ("x * 5 =", x * 5)
+print ("x / 5 =", x / 5)
+print ("x % 5 =", x % 5)
+print ("x // 5 =", x // 5)
+print ("x ^ 5 =", x ** 5)
